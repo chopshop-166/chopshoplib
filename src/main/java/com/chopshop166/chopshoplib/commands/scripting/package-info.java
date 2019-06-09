9880f078082caa07a86503d0bc15216f736096fc
Matt Soucy <git@msoucy.me>
2019-06-09 19:33:13 -0400

    Remove script engine commands

--- src/main/java/com/chopshop166/chopshoplib/commands/scripting/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Scripting framework for the command-based framework.
- * <p>
- * Warning, this library is experimental. It is subject to change at any time,
- * and may not be functional or useful for a team.
- */
-package com.chopshop166.chopshoplib.commands.scripting;
