22ed822ac456006b4f2f2a8886a26552a32a4ca1
Matt Soucy <git@msoucy.me>
2019-10-02 19:25:22 -0400

    Update CommandRobot.java

--- src/main/java/com/chopshop166/chopshoplib/CommandRobot.java
+++ src/main/java/com/chopshop166/chopshoplib/CommandRobot.java
@@ -11,31 +11,12 @@ public class CommandRobot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         // Do not call the super method, remove the annoying print
+        CommandScheduler.getInstance().run();
     }
 
     @Override
     public void disabledInit() {
         RobotUtils.resetAll(this);
-    }
-
-    @Override
-    public void disabledPeriodic() {
-        CommandScheduler.getInstance().run();
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-        CommandScheduler.getInstance().run();
-    }
-
-    @Override
-    public void teleopPeriodic() {
-        CommandScheduler.getInstance().run();
-    }
-
-    @Override
-    public void testPeriodic() {
-        // Do nothing special.
     }
 
 }
