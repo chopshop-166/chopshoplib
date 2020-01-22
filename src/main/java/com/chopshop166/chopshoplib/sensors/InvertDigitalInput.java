150cb5aef8c1764a05d2845a469b7e8a8b348eea
Andrew <amlefty5@gmail.com>
2020-01-21 20:26:20 -0500

    Made changes from comments
    Documentation fixes and logic edits

--- src/main/java/com/chopshop166/chopshoplib/sensors/InvertDigitalInput.java
+++ src/main/java/com/chopshop166/chopshoplib/sensors/InvertDigitalInput.java
@@ -5,30 +5,21 @@ import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
- * uses DigitalInputSource interface to create a invertable input source (cuz !
- * is boring)
+ * Invert Digital Input
+ * <p>
+ * creates boolean that is used to create a Digital Input {@link DigitalInput}
+ * that can be inverted based on the value passed into the boolean, adds
+ * functions that send and retrieve the inverted boolean
+ * 
+ * @author Andrew Martin
+ * @since 2020-01-20
  */
 public class InvertDigitalInput extends DigitalInput implements DigitalInputSource {
 
-/**
- * creates inverted boolean to be used for logic later
- */
-     * 
-     private boolean inverted;
-
-/**
- * allows input to change the inverted variable
- */
-    public void setInverted(boolean invertValue) {
-        inverted = invertValue;
-    }
-
     /**
-     * allows to check for inversion
+     * creates inverted boolean to be used for logic later
      */
-    public boolean getInverted() {
-        return inverted;
-    }
+    private boolean inverted;
 
     /**
      * creates our input via the source interface
@@ -38,13 +29,27 @@ public class InvertDigitalInput extends DigitalInput implements DigitalInputSour
     }
 
     /**
-     * Uses logic to use the inverted boolean to invert the returned value
+     * allows to check for inversion using the boolean created above
      */
-    @Override
+    public boolean getInverted() {
+        return inverted;
+    }
+
+    /**
+     * allows input to change the inverted variable
+     */
+    public void setInverted(boolean invertValue) {
+        inverted = invertValue;
+    }
+
+    /**
+     * Uses exclusive or to invert the returned value based on the inverted variable
+     */
+    public boolean get() {
+        return inverted ^ super.get();
+    }
+
     public boolean getAsBoolean() {
-        if (inverted == true) {
-            return !super.get();
-        } else
-            return super.get();
+        return get();
     }
 }
