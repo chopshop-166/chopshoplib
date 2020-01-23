6e2f0ed1213a6a095097038a294a1c92009b8071
Jeffrey Burke <jburke0519@gmail.com>
2020-01-22 20:27:22 -0500

    Changed for Pull-Request Comments

--- src/main/java/com/chopshop166/chopshoplib/maps/DifferentialDriveMap.java
+++ src/main/java/com/chopshop166/chopshoplib/maps/DifferentialDriveMap.java
@@ -1,22 +1,46 @@
 package com.chopshop166.chopshoplib.maps;
 
-import com.chopshop166.chopshoplib.outputs.SendableSpeedController;
+import com.chopshop166.chopshoplib.outputs.EncodedSpeedController;
+import com.chopshop166.chopshoplib.sensors.MockGyro;
 
 import edu.wpi.first.wpilibj.GyroBase;
 
 /**
  * Differential Drive Map
  * <p>
- * Gets the Left {@link SendableSpeedController}, gets the right
- * {@link SendableSpeedController}, and gets {@link Gyro}
+ * Provides a base map for differential drive and the left and right
+ * {@link EncodedSpeedController}. Also provides a {@link GyroBase}
  * 
  * @author Jeffrey Burke
  * @since 2020-01-20
  */
 public interface DifferentialDriveMap {
-    public SendableSpeedController getLeft();
+    /**
+     * Get Left
+     * <p>
+     * Provides the left {@link EncodedSpeedController}.
+     * 
+     * @return {@link EncodedSpeedController}
+     */
+    EncodedSpeedController getLeft();
 
-    public SendableSpeedController getRight();
+    /**
+     * Get Right
+     * <p>
+     * Provides the right {@link EncodedSpeedController}.
+     * 
+     * @return {@link EncodedSpeedController}
+     */
+    EncodedSpeedController getRight();
 
-    public GyroBase getGyro();
+    /**
+     * Geta a Gyro
+     * <p>
+     * Provides a mock gyro unless {@link EncodedSpeedController}.
+     * 
+     * @return Returns a {@link MockGyro}
+     */
+    default GyroBase getGyro() {
+        return new MockGyro();
+    };
 }
