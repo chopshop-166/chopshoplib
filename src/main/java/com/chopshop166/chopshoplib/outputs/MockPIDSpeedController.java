6662cdbab952eecf0cd8594d6aeb3119e9dae092
Matt Soucy <git@msoucy.me>
2020-02-14 20:54:10 -0500

    Added mock PID speed controller
    Nietzsche was right.

---
+++ src/main/java/com/chopshop166/chopshoplib/outputs/MockPIDSpeedController.java
@@ -0,0 +1,84 @@
+package com.chopshop166.chopshoplib.outputs;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+
+/**
+ * Mock speed controller that can be used to simulate a motor on a driver
+ * station, with "PID".
+ */
+public class MockPIDSpeedController implements PIDSpeedController {
+
+    private double speed;
+    private double setpoint;
+    private boolean isInverted;
+    private double p;
+    private double i;
+    private double d;
+
+    @Override
+    public void pidWrite(final double output) {
+        set(output);
+    }
+
+    @Override
+    public void set(final double speed) {
+        this.speed = isInverted ? -speed : speed;
+    }
+
+    @Override
+    public double get() {
+        return speed;
+    }
+
+    @Override
+    public void setInverted(final boolean isInverted) {
+        this.isInverted = isInverted;
+    }
+
+    @Override
+    public boolean getInverted() {
+        return isInverted;
+    }
+
+    @Override
+    public void disable() {
+        this.speed = 0.0;
+    }
+
+    @Override
+    public void stopMotor() {
+        this.speed = 0.0;
+    }
+
+    @Override
+    public void initSendable(final SendableBuilder builder) {
+        builder.setSmartDashboardType("Speed Controller");
+        builder.setActuator(true);
+        builder.setSafeState(this::disable);
+        builder.addDoubleProperty("Value", this::get, this::set);
+        builder.addDoubleProperty("Setpoint", () -> setpoint, this::setSetpoint);
+        builder.addDoubleProperty("P", () -> p, this::setP);
+        builder.addDoubleProperty("I", () -> i, this::setI);
+        builder.addDoubleProperty("D", () -> d, this::setD);
+    }
+
+    @Override
+    public void setP(final double kp) {
+        p = kp;
+    }
+
+    @Override
+    public void setI(final double ki) {
+        i = ki;
+    }
+
+    @Override
+    public void setD(final double kd) {
+        d = kd;
+    }
+
+    @Override
+    public void setSetpoint(final double setPoint) {
+        setpoint = setPoint;
+    }
+}
