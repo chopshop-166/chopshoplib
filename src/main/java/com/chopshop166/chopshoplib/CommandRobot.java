c6d026fcba6ec7d4ba66963eb799a66209456bcf
Matt Soucy <git@msoucy.me>
2020-02-26 20:04:43 -0500

    Create command group helpers

--- src/main/java/com/chopshop166/chopshoplib/CommandRobot.java
+++ src/main/java/com/chopshop166/chopshoplib/CommandRobot.java
@@ -1,6 +1,9 @@
 package com.chopshop166.chopshoplib;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
@@ -17,6 +20,30 @@ public class CommandRobot extends TimedRobot {
     @Override
     public void disabledInit() {
         RobotUtils.resetAll(this);
+    }
+
+    public static CommandBase sequence(final String name, final Command... first) {
+        CommandBase cmd = CommandGroupBase.sequence(first);
+        cmd.setName(name);
+        return cmd;
+    }
+
+    public static CommandBase parallel(final String name, final Command... cmds) {
+        CommandBase cmd = CommandGroupBase.parallel(cmds);
+        cmd.setName(name);
+        return cmd;
+    }
+
+    public static CommandBase race(final String name, final Command racers) {
+        CommandBase cmd = CommandGroupBase.race(racers);
+        cmd.setName(name);
+        return cmd;
+    }
+
+    public static CommandBase deadline(final String name, final Command limiter, final Command... cmds) {
+        CommandBase cmd = CommandGroupBase.deadline(limiter, cmds);
+        cmd.setName(name);
+        return cmd;
     }
 
 }
