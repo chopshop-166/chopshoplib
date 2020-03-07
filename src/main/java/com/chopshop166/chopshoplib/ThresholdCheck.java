8f2400e3134b5dbc585db231b299471c0c5d83b7
Matt Soucy <git@msoucy.me>
2020-03-06 20:17:25 -0500

    Create an IsFinished wrapper that triggers after a number of successes

---
+++ src/main/java/com/chopshop166/chopshoplib/ThresholdCheck.java
@@ -0,0 +1,41 @@
+package com.chopshop166.chopshoplib;
+
+import java.util.function.BooleanSupplier;
+
+/**
+ * Returns true if the given source is true a specified number of times in a
+ * row.
+ */
+public class ThresholdCheck implements BooleanSupplier {
+    final int nTimes;
+    final BooleanSupplier source;
+    int numSuccesses;
+
+    /**
+     * Returns true if the given source is true a specified number of times in a
+     * row.
+     * 
+     * @param nTimes The number of times for a match.
+     * @param source The operation to check.
+     * @return A function for use with isFinished.
+     */
+    public ThresholdCheck(int nTimes, BooleanSupplier source) {
+        this.nTimes = nTimes;
+        this.source = source;
+        reset();
+    }
+
+    public void reset() {
+        numSuccesses = 0;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        if (source.getAsBoolean()) {
+            numSuccesses++;
+        } else {
+            reset();
+        }
+        return numSuccesses >= nTimes;
+    }
+}
