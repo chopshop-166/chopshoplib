842abf3753f138319f54ba60d157aede83201cac
Matt Soucy <git@msoucy.me>
2020-03-08 00:17:49 -0400

    Factory command methods

--- src/main/java/com/chopshop166/chopshoplib/CommandRobot.java
+++ src/main/java/com/chopshop166/chopshoplib/CommandRobot.java
@@ -2,6 +2,8 @@ package com.chopshop166.chopshoplib;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -15,6 +17,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 /**
  * A Robot that calls the command scheduler in its periodic functions.
@@ -41,6 +47,68 @@ public class CommandRobot extends TimedRobot {
     @Override
     public void disabledInit() {
         RobotUtils.resetAll(this);
+    }
+
+    /**
+     * Create a {@link FunctionalCommand} with a name.
+     * 
+     * @param name       The command name.
+     * @param onInit     The action to run on initialize. If null, then will do
+     *                   nothing.
+     * @param onExecute  The action to run on execute. If null, then will do
+     *                   nothing.
+     * @param onEnd      The action to run on end. If null, then will do nothing.
+     * @param isFinished The condition to end the command.
+     * @return A new command.
+     */
+    public FunctionalCommand functional(final String name, final Runnable onInit, final Runnable onExecute,
+            final Consumer<Boolean> onEnd, final BooleanSupplier isFinished) {
+        final FunctionalCommand cmd = new FunctionalCommand(onInit == null ? () -> {
+        } : onInit, onExecute == null ? () -> {
+        } : onExecute, onEnd == null ? interrupted -> {
+        } : onEnd, isFinished);
+        cmd.setName(name);
+        return cmd;
+    }
+
+    /**
+     * Create an {@link InstantCommand}.
+     * 
+     * @param name   The name of the command.
+     * @param action The action to take.
+     * @return A new command.
+     */
+    public InstantCommand instant(final String name, final Runnable action) {
+        final InstantCommand cmd = new InstantCommand(action);
+        cmd.setName(name);
+        return cmd;
+    }
+
+    /**
+     * Create a {@link RunCommand}.
+     * 
+     * @param name   The name of the command.
+     * @param action The action to take.
+     * @return A new command.
+     */
+    public RunCommand running(final String name, final Runnable action) {
+        final RunCommand cmd = new RunCommand(action);
+        cmd.setName(name);
+        return cmd;
+    }
+
+    /**
+     * Create a {@link StartEndCommand}.
+     * 
+     * @param name    The name of the command.
+     * @param onStart The action to take on start.
+     * @param onEnd   The action to take on end.
+     * @return A new command.
+     */
+    public StartEndCommand startend(final String name, final Runnable onStart, final Runnable onEnd) {
+        final StartEndCommand cmd = new StartEndCommand(onStart, onEnd);
+        cmd.setName(name);
+        return cmd;
     }
 
     /**
