e700f594522198852e36cb8ab727933fae2ff96c
Matt Soucy <git@msoucy.me>
2021-01-20 22:20:15 -0500

    Fix PMD

--- src/main/java/com/chopshop166/chopshoplib/commands/SmartSubsystem.java
+++ src/main/java/com/chopshop166/chopshoplib/commands/SmartSubsystem.java
@@ -6,7 +6,6 @@ import java.util.function.Consumer;
 import com.chopshop166.chopshoplib.Resettable;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
