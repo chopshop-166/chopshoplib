c9320c059b2abe6e8ca0ae03d712e847e07ec459
Matt Soucy <git@msoucy.me>
2021-06-22 19:17:23 -0400

    Preset Wait For Setpoint command and persistence check

--- src/main/java/com/chopshop166/chopshoplib/commands/PresetSubsystem.java
+++ src/main/java/com/chopshop166/chopshoplib/commands/PresetSubsystem.java
@@ -2,6 +2,7 @@ package com.chopshop166.chopshoplib.commands;
 
 import java.util.function.DoubleSupplier;
 
+import com.chopshop166.chopshoplib.PersistenceCheck;
 import com.chopshop166.chopshoplib.Resettable;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
@@ -9,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 /**
  * A {@link Subsystem} that is {@link Resettable} and that provides convenience
@@ -17,13 +19,18 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 public abstract class PresetSubsystem<T extends Enum<?> & DoubleSupplier> extends PIDSubsystem
         implements SmartSubsystem {
 
+    /** Check to make sure it's at the setpoint for enough time */
+    private final PersistenceCheck persistenceCheck;
+
     /**
      * Construct the subsystem.
      * 
      * @param controller The PID controller to use.
+     * @param numSamples Number of setpoint valid samples to check for.
      */
-    public PresetSubsystem(final PIDController controller) {
+    public PresetSubsystem(final PIDController controller, final int numSamples) {
         super(controller);
+        persistenceCheck = new PersistenceCheck(numSamples, controller::atSetpoint);
     }
 
     /**
@@ -31,9 +38,11 @@ public abstract class PresetSubsystem<T extends Enum<?> & DoubleSupplier> extend
      * 
      * @param controller The PID controller to use.
      * @param initial    The initial value.
+     * @param numSamples Number of setpoint valid samples to check for.
      */
-    public PresetSubsystem(final PIDController controller, final double initial) {
+    public PresetSubsystem(final PIDController controller, final int numSamples, final double initial) {
         super(controller, initial);
+        persistenceCheck = new PersistenceCheck(numSamples, controller::atSetpoint);
     }
 
     /**
@@ -46,6 +55,27 @@ public abstract class PresetSubsystem<T extends Enum<?> & DoubleSupplier> extend
         return new InstantCommand(() -> {
             setSetpoint(value.getAsDouble());
         }, this).withName("Set to " + value.name());
+    }
+
+    /**
+     * Wait until the subsystem is moved to the proper setpoint.
+     * 
+     * @return The wait command.
+     */
+    public CommandBase waitForSetpoint() {
+        final CommandBase ret = new WaitUntilCommand(persistenceCheck).withName("Wait For Setpoint");
+        ret.addRequirements(this);
+        return ret;
+    }
+
+    /**
+     * Set the preset for the subsystem, and wait until it moves to that point.
+     * 
+     * @param value The preset to use.
+     * @return The instant command.
+     */
+    public CommandBase presetWait(final T value) {
+        return CommandRobot.sequence("Set to " + value.name(), presetCmd(value), waitForSetpoint());
     }
 
     @Override
