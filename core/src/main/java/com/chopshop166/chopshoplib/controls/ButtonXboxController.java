07a61984c0aa94e016300bf818f1f818bdc9e38b
Matt Soucy <git@msoucy.me>
2021-11-18 20:41:27 -0500

    Fix triggers

--- core/src/main/java/com/chopshop166/chopshoplib/controls/ButtonXboxController.java
+++ core/src/main/java/com/chopshop166/chopshoplib/controls/ButtonXboxController.java
@@ -3,6 +3,8 @@ package com.chopshop166.chopshoplib.controls;
 import java.util.EnumMap;
 import java.util.Map;
 
+import com.chopshop166.chopshoplib.triggers.XboxTrigger;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
@@ -11,66 +13,6 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
  * Represents an XBox controller along with its associated buttons.
  */
 public class ButtonXboxController extends XboxController {
-
-    /** The mapping of button ID to command button. */
-    private final Map<Button, JoystickButton> buttons = new EnumMap<>(Button.class);
-
-    /** The mapping of POV Button direction and command button */
-    private final Map<POVDirection, POVButton> povButtons = new EnumMap<>(POVDirection.class);
-
-    /**
-     * Construct an instance of a Xbox Controller along with each button the
-     * joystick has.
-     *
-     * @param port The USB port that the Xbox Controller is connected to on the
-     *             Driver Station
-     */
-    public ButtonXboxController(final int port) {
-        super(port);
-    }
-
-    /**
-     * Get a button from this Xbox Controller
-     * <p>
-     * Returns the sepcified button of a Xbox Controller without having to
-     * explicitly create each button.
-     * 
-     * @param buttonId The index of the button to accesss
-     * @return The button object for the given ID
-     */
-    public JoystickButton getButton(final Button buttonId) {
-        if (!buttons.containsKey(buttonId)) {
-            buttons.put(buttonId, new JoystickButton(this, buttonId.value));
-        }
-        return buttons.get(buttonId);
-    }
-
-    /**
-     * Get the right trigger - left trigger
-     * 
-     * @return A double in {@code [-1, 1]}
-     */
-    public double getTriggers() {
-        final double kRight = getTriggerAxis(Hand.kRight);
-        final double kLeft = getTriggerAxis(Hand.kLeft);
-        return kRight - kLeft;
-    }
-
-    /**
-     * Get a button from the POV hat on this Xbox Controller
-     * <p>
-     * Returns the sepcified POV Hat button of an Xbox controller without having to
-     * explicitly create each button.
-     * 
-     * @param angle The index of the button to accesss
-     * @return The button object for the given ID
-     */
-    public POVButton getPovButton(final POVDirection angle) {
-        if (!povButtons.containsKey(angle)) {
-            povButtons.put(angle, new POVButton(this, angle.getAngle()));
-        }
-        return povButtons.get(angle);
-    }
 
     /**
      * Enum of POV HAT directions
@@ -90,5 +32,75 @@ public class ButtonXboxController extends XboxController {
         POVDirection(final int rotation) {
             this.dPadRotation = rotation;
         }
+    }
+
+    /** The mapping of button ID to command button. */
+    private final Map<Button, JoystickButton> buttons = new EnumMap<>(Button.class);
+
+    /** The mapping of POV Button direction to command button. */
+    private final Map<POVDirection, POVButton> povButtons = new EnumMap<>(POVDirection.class);
+
+    /** The mapping of hand to command button. */
+    private final Map<Hand, XboxTrigger> triggerButtons = new EnumMap<>(Hand.class);
+
+    /**
+     * Construct an instance of a Xbox Controller along with each button the
+     * joystick has.
+     *
+     * @param port The USB port that the Xbox Controller is connected to on the
+     *             Driver Station.
+     */
+    public ButtonXboxController(final int port) {
+        super(port);
+    }
+
+    /**
+     * Get the right trigger - left trigger
+     * 
+     * @return A double in {@code [-1, 1]}
+     */
+    public double getTriggers() {
+        final double kRight = getTriggerAxis(Hand.kRight);
+        final double kLeft = getTriggerAxis(Hand.kLeft);
+        return kRight - kLeft;
+    }
+
+    /**
+     * Get a button from this Xbox Controller.
+     * <p>
+     * Returns the sepcified button of a Xbox Controller without having to
+     * explicitly create each button.
+     * 
+     * @param buttonId The index of the button to access.
+     * @return The button object for the given ID.
+     */
+    public JoystickButton getButton(final Button buttonId) {
+        return buttons.computeIfAbsent(buttonId, b -> new JoystickButton(this, b.value));
+    }
+
+    /**
+     * Get a trigger from this Xbox Controller.
+     * <p>
+     * Returns the sepcified trigger of a Xbox Controller without having to
+     * explicitly create each one.
+     * 
+     * @param hand The hand of the trigger to access.
+     * @return The trigger object for the given hand.
+     */
+    public XboxTrigger getTrigger(final Hand hand) {
+        return triggerButtons.computeIfAbsent(hand, h -> new XboxTrigger(this, hand));
+    }
+
+    /**
+     * Get a button from the POV hat on this Xbox Controller.
+     * <p>
+     * Returns the sepcified POV Hat button of an Xbox controller without having to
+     * explicitly create each button.
+     * 
+     * @param angle The index of the button to access.
+     * @return The button object for the given ID.
+     */
+    public POVButton getPovButton(final POVDirection angle) {
+        return povButtons.computeIfAbsent(angle, a -> new POVButton(this, a.getAngle()));
     }
 }
