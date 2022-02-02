9d8e4a9da9813153e725acbf159813c2a6d223bc
Matt Soucy <git@msoucy.me>
2022-02-01 20:24:30 -0500

    Modifier group constructor

--- core/src/main/java/com/chopshop166/chopshoplib/motors/ModifierGroup.java
+++ core/src/main/java/com/chopshop166/chopshoplib/motors/ModifierGroup.java
@@ -4,26 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.DoubleUnaryOperator;
 
 /**
- * A function from double to double, converting and limiting speeds.
+ * A group of {@link Modifier} objects.
  */
 public class ModifierGroup {
 
     /** The actual modifier list. */
-    private final List<Modifier> modifiers = new ArrayList<>();
+    private final List<DoubleUnaryOperator> modifiers = new ArrayList<>();
+
+    /**
+     * Create group with modifiers.
+     * 
+     * @param ms Modifiers to start with.
+     */
+    public ModifierGroup(final DoubleUnaryOperator... ms) {
+        modifiers.addAll(Arrays.asList(ms));
+    }
 
     /**
      * Run all modifiers.
      *
-     * As modifiers could have side effects, this is private.
-     *
-     * @param rawSpeed The base speed to run
-     * @return The new speed
+     * @param rawSpeed The base speed to start from.
+     * @return The new speed.
      */
     public double run(final double rawSpeed) {
         double speed = rawSpeed;
-        for (final Modifier m : modifiers) {
+        for (final DoubleUnaryOperator m : modifiers) {
             speed = m.applyAsDouble(speed);
         }
         return speed;
