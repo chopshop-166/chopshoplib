aa9774e51ce90d9b13544678ff02c612eb9b9598
Matt Soucy <git@msoucy.me>
2022-02-25 19:04:20 -0500

    Linear direction modifier functions

--- core/src/main/java/com/chopshop166/chopshoplib/states/LinearDirection.java
+++ core/src/main/java/com/chopshop166/chopshoplib/states/LinearDirection.java
@@ -5,9 +5,50 @@ package com.chopshop166.chopshoplib.states;
  */
 public enum LinearDirection {
     // Reverse Direction
-    REVERSE,
+    REVERSE {
+        /**
+         * Get a value (usually speed) adjusted for direction.
+         * 
+         * @param value The input value.
+         * @return The inverted value.
+         */
+        @Override
+        public double get(final double value) {
+            return -value;
+        }
+    },
     // Neither forward nor reverse
-    NEUTRAL,
+    NEUTRAL {
+        /**
+         * Cancel a speed for neutral state.
+         * 
+         * @param value The input value.
+         * @return No speed (0.0).
+         */
+        @Override
+        public double get(final double value) {
+            return 0.0;
+        }
+    },
     // Forward Direction
-    FORWARD
+    FORWARD {
+        /**
+         * Get a value (usually speed) adjusted for direction.
+         * 
+         * @param value The input value.
+         * @return The same value.
+         */
+        @Override
+        public double get(final double value) {
+            return value;
+        }
+    };
+
+    /**
+     * Get the given speed, inverting if necessary.
+     * 
+     * @param value The input speed.
+     * @return The input speed or its inverted value.
+     */
+    public abstract double get(final double value);
 }
