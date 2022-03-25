659f3b90a5796aa998565372571e32c0e2db8f9b
Matt Soucy <git@msoucy.me>
2022-03-25 19:07:41 -0400

    Documentation

--- core/src/main/java/com/chopshop166/chopshoplib/digital/DigitalInputSource.java
+++ core/src/main/java/com/chopshop166/chopshoplib/digital/DigitalInputSource.java
@@ -4,10 +4,9 @@ import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
- * A {@link DigitalInput} that acts as a {@link BooleanSupplier}.
+ * A wrapper for {@link BooleanSupplier} that can be sent to the dashboard.
  */
 @FunctionalInterface
 public interface DigitalInputSource extends Sendable, BooleanSupplier {
