829cdf95ba372a06de0a30acd8df217396cf444a
Matt Soucy <git@msoucy.me>
2023-12-12 18:47:40 -0500

    fix: Re-add Build Command to make Ben less sad

--- core/src/main/java/com/chopshop166/chopshoplib/commands/CommandUtils.java
+++ core/src/main/java/com/chopshop166/chopshoplib/commands/CommandUtils.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WrapperCommand;
 
 /**
@@ -39,6 +40,27 @@ final public class CommandUtils {
      */
     public static Command repeat(final int numTimesToRun, final Supplier<Command> cmd) {
         return Commands.sequence(Stream.generate(cmd).limit(numTimesToRun).toArray(Command[]::new));
+    }
+
+    /**
+     * Create a command builder with a given name.
+     *
+     * @param requirements The subsystems that the command needs (can be empty).
+     * @return A new command builder.
+     */
+    public static BuildCommand cmd(final Subsystem... requirements) {
+        return new BuildCommand(requirements);
+    }
+
+    /**
+     * Create a command builder with a given name.
+     *
+     * @param name The command name.
+     * @param requirements The subsystems that the command needs (can be empty).
+     * @return A new command builder.
+     */
+    public static BuildCommand cmd(final String name, final Subsystem... requirements) {
+        return new BuildCommand(name, requirements);
     }
 
     /**
