4d3bec46c1a9fd34b3c89dcefdad39298c82da7a
Matt Soucy <git@msoucy.me>
2023-12-19 18:13:05 -0500

    Revert "Remove reflection-based map detection"
    This reverts commit 3788b49c5de01ef07327c4d1d56b5edfaf8e6f08.

---
+++ core/src/test/java/com/chopshop166/chopshoplib/maps/MapA.java
@@ -0,0 +1,12 @@
+package com.chopshop166.chopshoplib.maps;
+
+/**
+ * Sample RobotMap for the robot A.
+ */
+@RobotMapFor("A")
+public class MapA implements RobotMap {
+    @Override
+    public MapType getType() {
+        return MapType.A;
+    }
+}
