99d34d773df149f2361499486af20cb9b495f897
Matt Soucy <git@msoucy.me>
2024-02-03 14:45:57 -0500

    fix: Actually use the data layer

--- core/src/main/java/com/chopshop166/chopshoplib/logging/data/DifferentialDriveData.java
+++ core/src/main/java/com/chopshop166/chopshoplib/logging/data/DifferentialDriveData.java
@@ -2,11 +2,15 @@ package com.chopshop166.chopshoplib.logging.data;
 
 import com.chopshop166.chopshoplib.logging.DataWrapper;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive.WheelSpeeds;
+
 /**
  * Differential Drive Data
  *
- * Provides an interface for getting and setting data in the Different Drive Map. This implemnts the
- * loggable IO layer that AdvantageKit expects and provides a snapshot of sensor input
+ * Provides an interface for getting and setting data in the Different Drive
+ * Map. This implemnts the loggable IO layer that AdvantageKit expects and
+ * provides a snapshot of sensor input.
  */
 public class DifferentialDriveData extends DataWrapper {
 
@@ -17,4 +21,28 @@ public class DifferentialDriveData extends DataWrapper {
 
     /** The Yaw angle of the robot in Degrees */
     public double gyroYawAngleDegrees;
+
+    /**
+     * Use arcade style drive.
+     * 
+     * @param xSpeed    The x speed.
+     * @param zRotation The robot rotation.
+     */
+    public void arcadeDrive(final double xSpeed, final double zRotation) {
+        final WheelSpeeds speeds = DifferentialDrive.arcadeDriveIK(xSpeed, zRotation, true);
+        this.left.setpoint = speeds.left;
+        this.right.setpoint = speeds.right;
+    }
+
+    /**
+     * Use tank style drive.
+     * 
+     * @param leftSpeed  The left motor speed.
+     * @param rightSpeed The right motor speed.
+     */
+    public void tankDrive(final double leftSpeed, final double rightSpeed) {
+        final WheelSpeeds speeds = DifferentialDrive.tankDriveIK(leftSpeed, rightSpeed, false);
+        this.left.setpoint = speeds.left;
+        this.right.setpoint = speeds.right;
+    }
 }
